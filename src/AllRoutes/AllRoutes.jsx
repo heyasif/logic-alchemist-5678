4caa03976e9e497fcae3bc3c64414b1731186746
RantuBangal <rantubangal996@gmail.com>
2024-02-22 20:12:13 +0530

    Partial Home Component

--- src/AllRoutes/AllRoutes.jsx
+++ src/AllRoutes/AllRoutes.jsx
@@ -1,18 +1,16 @@
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from '../Home';
-import Error from '../Error';
+import { Routes, Route } from 'react-router-dom';
+import Home from '../Rantu/Home';
+import Error from '../Rantu/Error';
 
 export default function AllRoutes(){
 
     return(
         <div>
-            <BrowserRouter>
               <Routes>
                 <Route path='/' element={<Home />} />
                 <Route path='*' element={<Error/>} />
               </Routes>
-            </BrowserRouter>
         </div>
     )
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom';
+
+
 import './index.css'
 import { ChakraProvider } from '@chakra-ui/react'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ChakraProvider>
-    <App />
-    </ChakraProvider>
+    <BrowserRouter>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </BrowserRouter>
   </React.StrictMode>
 )
