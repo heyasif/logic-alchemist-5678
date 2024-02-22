54196e4d0e7d14b27cd09cd9d8cf601b020fed97
raushanrauss <151566432+raushanrauss@users.noreply.github.com>
2024-02-23 00:54:32 +0530

    login_partial_done

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,18 @@
 import './App.css'
 import Home from './Rantu/Home'
 import Navbar from './Rantu/Navbar'
-
+import  Cart from './Raushan/Cart'
+import Signup from './Raushan/Signup'
+import Login from './Raushan/login'
 function App() {
 
 
   return (
     <>
+    <Signin />
+    {/* <Cart />
     <Navbar />
-     <Home />
+     <Home /> */}
     </>
   )
 }
