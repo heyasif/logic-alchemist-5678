a04c6f63c2f8546344a667b94af2930273e7096c
SUJU1998 <111004060+SUJU1998@users.noreply.github.com>
2024-02-23 00:50:26 +0530

    in Process

--- src/AllRoutes/AllRoutes.jsx
+++ src/AllRoutes/AllRoutes.jsx
@@ -3,18 +3,18 @@ import { Routes, Route } from 'react-router-dom';
 import Home from '../Rantu/Home';
 import Error from '../Rantu/Error';
 import Account from '../Asif/Account';
-import SinglePage from '../sujeet/SinglePage';
 import Cart from '../Raushan/Cart';
+import SinglePage from '../sujeet/singlePage';
 
 export default function AllRoutes(){
 
     return(
         <div>
               <Routes>
-                <Route path='/' element={<Home />} />
-                <Route path='/account' element={<Account />} />
-                <Route path='/products' element={<SinglePage />} />
-                <Route path='/login' element={<Cart />}/>
+                <Route path='/' element={<Home/>} />
+                <Route path='/account' element={<Account/>} />
+                <Route path='/products' element={<SinglePage/>} />
+                <Route path='/login' element={<Cart/>}/>
                 <Route path='*' element={<Error/>} />
               </Routes>
         </div>
